74f31055f174cb1d047b5cefc92b39b6010e0cd9
madonna-mosaad <madonnamosaad86@gmail.com>
2024-07-19 02:02:30 +0300

    part01

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace OOP04
+﻿using OOP04.interfaces;
+using System.Text;
+
+namespace OOP04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+
+            #region part1
+            Employee e1 = new Employee()
+            {
+                ID = 100,
+                Name = "madonna"
+            };
+            Employee e2 = new Employee()
+            {
+                ID = 20,
+                Name = "mirna"
+            };
+            Employee e3 = new Employee()
+            {
+                ID = 3,
+                Name = "mariam"
+            };
+            Employee[] e ={ e1, e2 };
+            Employee[] emp = new Employee[2];
+            #region shallow and deep copy
+            //emp = e;//shallow copy
+            //emp[0] = e3;
+            //Console.WriteLine(e[0]); // has same change that happend in emp
+            //emp =(Employee[])emp.Clone();//deep copy
+            //emp[0] = e2;
+            //Console.WriteLine(e[0]); // has not same change that happend in emp
+            #endregion
+            #region sort
+            //Array.Sort(e);
+            //foreach (Employee employee in e)
+            //{
+            //    Console.WriteLine(employee);
+            //}
+            #endregion
+            #endregion
+
+            
         }
     }
 }
---
+++ OOP04/interfaces/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.interfaces
+{
+    internal class Employee : IUnderGrade, IGrade, ICloneable, IComparable
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public object Clone()
+        {
+            return new Employee()
+            {
+                ID = this.ID,
+                Name = this.Name
+            };
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Employee Emp =(Employee)obj;
+            return this.ID.CompareTo(Emp.ID);
+        }
+
+        public void degree()
+        {
+            Console.WriteLine("degree");
+        }
+
+        void IGrade.GradeYear()
+        {
+            Console.WriteLine("grade");
+        }
+        void IUnderGrade.GradeYear()
+        {
+            Console.WriteLine("under grade");
+        }
+        public override string ToString()
+        {
+            return $"{Name} {ID}";
+        }
+    }
+}
